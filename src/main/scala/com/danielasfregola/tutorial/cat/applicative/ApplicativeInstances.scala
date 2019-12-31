f61980950853d4d1e4dad3f7cab59b43ada7052b
Oriol López Massaguer <oriol.lopez@chemotargets.com>
2019-12-31 16:47:36 +0100

    monoid functor

--- src/main/scala/com/danielasfregola/tutorial/cat/applicative/ApplicativeInstances.scala
+++ src/main/scala/com/danielasfregola/tutorial/cat/applicative/ApplicativeInstances.scala
@@ -6,8 +6,29 @@ import com.danielasfregola.tutorial.cat._
 
 object ApplicativeInstances {
 
-  implicit val maybeApplicative: Applicative[Maybe] = ???
+  implicit val maybeApplicative: Applicative[Maybe] = new Applicative[Maybe] {
+    override def pure[A](a: A): Maybe[A] = Just(a)
 
-  implicit val zeroOrMoreApplicative: Applicative[ZeroOrMore] = ???
+    override def ap[A, B](boxF: Maybe[A => B])(boxA: Maybe[A]): Maybe[B] =
+      (boxA,boxF) match {
+      case (Empty,_) => Empty
+      case (_,Empty) => Empty
+      case (Just(a),Just(f)) =>  Just(f(a))
+    }
+  }
 
-}
+  implicit val zeroOrMoreApplicative: Applicative[ZeroOrMore] = new Applicative[ZeroOrMore] {
+    override def pure[A](a: A): ZeroOrMore[A] = OneOrMore(a, Zero)
+
+    override def ap[A, B](boxF: ZeroOrMore[A => B])(boxA: ZeroOrMore[A]): ZeroOrMore[B] = (boxA, boxF) match {
+      case (Zero, _) => Zero
+      case (_, Zero) => Zero
+      case (OneOrMore(a, Zero), OneOrMore(f, fs)) => OneOrMore(f(a), ap(fs)(boxA))
+      case (OneOrMore(a,as), OneOrMore(f,Zero)) => OneOrMore(f(a),Zero)
+      case (OneOrMore(a,as), OneOrMore(f,fs)) =>{
+
+      }
+
+    }
+    }
+  }
